9708cfef4e5f29f139bf6cfb2a086b40d5b6a348
sunkakar <sundeepkakar@gmail.com>
2018-12-29 14:32:09 +0530

    Added Functional Scrolling

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ $(document).ready(function(){
   $("#linkedin-tab").click(function(){alert("linkedin-tab");})
 
   $("#github-tab").click(function(){alert("github-tab");})
+
+  let init_header_position = $('#header').offset().top
+  let header = $("#header")
+
+  $(window).on("scroll touchmove", function() {
+    let scroll = $(window).scrollTop()
+    console.log(scroll);
+    if(scroll >= init_header_position){
+      header.removeClass('sticky');
+      header.addClass('fixed');
+    } else {
+      header.removeClass('fixed');
+      header.addClass('sticky');
+    }
+  })
 })
